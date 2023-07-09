2d8eaec199df6bbb86c28d0d7be7ce4d0e1ca186
SaumyaRana21 <129658554+SaumyaRana21@users.noreply.github.com>
2023-07-10 03:24:10 +0530

    Update Snake_Water_Gun_Basic.py

--- Snake_Water_Gun_Basic.py
+++ Snake_Water_Gun_Basic.py
@@ -1,46 +1,38 @@
-import random
-from tkinter import *
+import random 
+print("WELCOME TO THE GAME:- ")
+print("First is computer's chance to choose\n   \n Now its your chance to choose") 
+print("\n")
+user_input=input('''OPTIONS: Choose S for Snake \n Choose W for Water \n Choose G for Gun \n Your option: ''')
+print()
 
-# Game logic function
-def play_game(user_choice):
-    options = ["Snake", "Water", "Gun"]
-    computer_choice = random.choice(options)
+comp=random.randrange(1,4)
+if comp==1:
+    Comp='S'
+elif comp==2:
+    comp='W'
+else:
+    comp='G'
+print("computer choosed",comp)
+def gamewin():
+    if user_input==Comp:
+        print("It's a Tie !")
+    elif user_input=='W':
+        if comp=='S':
+            return False
+        elif comp=='G':
+            return True
+    elif user_input=='G':
+        if comp=='S':
+            return True
+        elif comp=='G':
+            return False
+    elif user_input=='S':
+        if comp=='W':
+            return True
+        elif comp=='G':
+            return False
 
-    result = ""
-    if user_choice == computer_choice:
-        result = "It's a tie!"
-    elif (user_choice == "Snake" and computer_choice == "Water") or (user_choice == "Water" and computer_choice == "Gun") or (user_choice == "Gun" and computer_choice == "Snake"):
-        result = "You won!"
-    else:
-        result = "You lost!"
-
-    result_label.configure(text=f"You chose: {user_choice}\nComputer chose: {computer_choice}\nResult: {result}")
-
-
-# Function to handle user input
-def get_user_input():
-    user_choice = input_var.get()
-    play_game(user_choice)
-
-# Create Tkinter window
-window = Tk()
-window.title("Snake Water Gun")
-
-# Label and Entry for user input
-input_label = Label(window, text="OPTIONS: Choose S for Snake, W for Water, G for Gun")
-input_label.pack()
-
-input_var = StringVar()
-input_entry = Entry(window, textvariable=input_var)
-input_entry.pack()
-
-# Button to submit user input
-submit_button = Button(window, text="Submit", command=get_user_input)
-submit_button.pack()
-
-# Result label
-result_label = Label(window)
-result_label.pack()
-
-# Start Tkinter event loop
-window.mainloop()
+if True:
+    print("You WON !")
+else:
+    print("You Lost !")
